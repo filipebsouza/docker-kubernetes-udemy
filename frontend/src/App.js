50c0f10b98b98e7114e61685cc7119b222378127
Filipe Bezerra de Souza <filipe.souza@db1.com.br>
2020-04-14 17:56:53 -0300

    Aula 88

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         ddddddd
+         Olha só esse cara!
         </p>
         <a
           className="App-link"
