c38fb747b4eec626b61331659a98e900c0bf0319
Filipe Bezerra de Souza <filipe.souza@db1.com.br>
2020-05-25 21:38:03 -0300

    Alteracoes do docker compose do complex

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -10,11 +10,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
